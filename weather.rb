76501bba9e643989ebf42a7001353b799d9f4b3f
Denis <icestrelok@gmail.com>
2019-08-07 23:26:21 +0300

    weather-simple-parse :: Meteoservice.ru

--- weather.rb
+++ weather.rb
@@ -1,3 +1,9 @@
+
+#....................................................................#
+#                                                                    #
+# Прогноз погоды от сайта Метеосервис. Предоставлено Meteoservice.ru #
+#                                                                    #
+# ...................................................................#
 if Gem.win_platform?
   Encoding.default_external = Encoding.find(Encoding.locale_charmap)
   Encoding.default_internal = __ENCODING__
@@ -12,6 +18,8 @@ require 'uri'
 require 'rexml/document'
 
 THE_CLOUDS = {0=>'Ясно', 1=>'Малооблачно', 2=>'Облачно', 3=>'Пасмурно'}
+THE_PEECIPITATIAN = {3=>'смешанные', 4=>'дождь', 5=>'ливень',
+                     6=>'снег', 7=>'снег', 8=>'гроза', 9=>'нет данных', 10=>'без осадков'}
 
 uri = URI.parse("https://xml.meteoservice.ru/export/gismeteo/point/69.xml")
 
@@ -32,8 +40,25 @@ max_wind = current_forecast.elements['WIND'].attributes['max']
 
 clouds_index = current_forecast.elements['PHENOMENA'].attributes['cloudiness'].to_i
 clouds = THE_CLOUDS[clouds_index]
+precipitation_index = current_forecast.elements['PHENOMENA'].attributes['precipitation'].to_i
+precipitation = THE_PEECIPITATIAN[precipitation_index]
 
-puts city_name
-puts "температура от #{min_temp} до #{max_temp}"
+
+pressure = current_forecast.elements['PRESSURE'].attributes['max'].to_s
+
+relwet = current_forecast.elements['RELWET'].attributes['max'].to_s
+
+forecast = doc.root.elements["REPORT/TOWN/FORECAST"].attributes
+month = current_forecast.attributes['month']
+day   = current_forecast.attributes['day']
+##precipitation - тип осадков:
+# 3 - смешанные, 4 - дождь, 5 - ливень, 6,7 – снег, 8 - гроза, 9 - нет данных, 10 - без осадков
+
+puts "Прогноз погоды от сайта Метеосервис\nпредоставлено Meteoservice.ru\n\n\n"
+puts "#{city_name} [день:#{day} месяц:#{month}"
+puts "Температура от #{min_temp} до #{max_temp}"
 puts "Ветер: #{max_wind} m/s"
 puts clouds
+puts "Осадки: #{precipitation}"
+puts "Давление (max): #{pressure}mm"
+puts "Влажность: #{relwet}%"
